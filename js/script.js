daf264273f445c0ca9badf37981d31a511b70488
waelragab <wael@student.aast.edu>
2020-07-17 23:11:59 +0200

    project added

---
+++ js/script.js
@@ -0,0 +1,80 @@
+const synth = window.speechSynthesis;
+
+
+const textForm = document.querySelector("form");
+const textInput = document.querySelector("#text-input");
+const voiceSelect = document.querySelector("#voice-select");
+const rate = document.querySelector("#rate");
+const rateValue = document.querySelector("#rate-value");
+const pitch = document.querySelector("#pitch");
+const pitchValue = document.querySelector("#pitch-value");
+
+
+let voices = [];
+
+const getVoices = () => {
+    voices = synth.getVoices();
+
+    voices.forEach(voice =>{
+    const option = document.createElement("option");
+    option.textContent = voice.name + "(" +voice.lang+ ")";
+    option.setAttribute("data-lang", voice.lang);
+    option.setAttribute("data-name", voice.name);
+
+
+    voiceSelect.appendChild(option);
+    })
+    
+}
+getVoices();
+
+if(synth.onvoiceschanged !== undefined){
+    synth.onvoiceschanged = getVoices;
+}
+
+const speak = () =>{
+    if(synth.speaking){
+        console.error("Already speaking");
+        return;
+    }
+    if(textInput.value !== ""){
+        const speakText = new SpeechSynthesisUtterance(textInput.value);
+        
+
+    speakText.onend = e => {
+        console.log("done speaking...");
+    }
+
+    speakText.onerror = e => {
+        console.error("something went erong");
+    }
+
+    const selectedVoice = voiceSelect.selectedOptions[0].getAttribute('data-name');
+
+    voices.forEach(voice => {
+        if(voice.name === selectedVoice){
+            speakText.voice = voice;
+        }
+    });
+    
+
+    //set pitch and rate
+    speakText.rate = rate.value;
+    speakText.pitch = pitch.value;
+
+    synth.speak(speakText)
+   }
+}
+
+//Event listener
+
+textForm.addEventListener("submit", e=>{
+    e.preventDefault();
+    speak();
+    textInput.blur();
+});
+
+rate.addEventListener("change", e => rateValue.textContent = rate.value)
+pitch.addEventListener("change", e => pitchValue.textContent = pitch.value)
+
+voiceSelect.addEventListener("change", e => speak());
